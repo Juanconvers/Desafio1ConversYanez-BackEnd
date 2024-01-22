3b2c95c9ed48f1c5f473d86c42d7fb2faea209af
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2024-01-22 06:23:55 -0500

    Commit No.2: Creación objeto product

--- ProductManager.js
+++ ProductManager.js
@@ -1,23 +1,47 @@
 
 import crypto from 'crypto'
+
 class ProductManager {
     constructor(){
         this.products = [];
-        
+  
     }
 
     addProduct(producto){
         // validar que todos los datos se hayan ingresado
          
-         const existe = this.products.includes(prod => prod.code === producto.code)
+        function producto (title,description,price,thumbnail,code,stock) {
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.thumbnail = thumbnail;
+            this.code = code;
+            this.stock = stock;
+            this.id = id;
+        }
+
+            const existe = this.products.includes(prod => prod.code === producto.code)
          
          if (existe){
             return "El producto ya existe"
          } else {
-            producto.id = crypto.randomBytes(10).toString('hex')
+            const producto1 = new producto({
+                title: "producto prueba", 
+                description: "Este es un producto prueba", 
+                price: 200, 
+                Thumbnail: "Sin imagen", 
+                code: "abc123", 
+                stock: 25,
+                id: crypto.randomBytes(10).toString('hex')
+            })
+            console.log("Producto creado exitosamente")
+            
             this.products.push(producto)
+            console.log("Producto agregado exitosamente")
          }
     }
 
+    
 
 }
+console.log(ProductManager.products)
