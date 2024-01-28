fad340bf674460840b6f3e4289a440489bb1d0c7
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2024-01-27 21:45:05 -0500

    Commit 4: Inicio Corrección codigo para reentrega

--- ProductManager.js
+++ ProductManager.js
@@ -1,43 +1,56 @@
 
 import crypto from 'crypto'
 
-const producto = {
-    title: "", 
-    description: "", 
-    price: 0, 
-    thumbnail: "", 
-    code: "", 
-    stock: 0,
-}
-
 class ProductManager {
     constructor() {
         this.products = [];
     }
 
-    addProduct(producto) {
-        if (this.products.includes(prod => prod.code === producto.code)) {
-            return "El producto ya existe"
-        } else {
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("Todos los campos deben ser llenados");
+            return;
+        } if (this.products.some(product => product.code === code)){
+            this.console.error("Este producto ya existe");
+            return;
+        } 
+
+        const NEWPRODUCT = {
+            id: this.products.length + 1, // crypto.randomBytes().toString('hex')
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        };
+
+        this.products.push(NEWPRODUCT);
+        console.log("Producto ", NEWPRODUCT.title, " fue agregado exitosamente");
+
+
+
+        const producto = {
+            title: "", 
+            description: "", 
+            price: 0, 
+            thumbnail: "", 
+            code: "", 
+            stock: 0,
+        }
+        
+    }
+
+    getProducts() {
+        return console.log(ProductManager.products)
+    }
+    
+    getProductById(){
+        if (ProductManager.products.filter(x => x.id === $(id))){
             
         }
-
-        console.log("Producto creado exitosamente")
-        producto.id = crypto.randomBytes().toString('hex')
-        this.products.push(producto)
-        console.log("Producto agregado exitosamente")
     }
 }
-
-getProducts = () => {
-    console.log(ProductManager.products)
-}
-
-getProductById = () => {
-    ProductManager.products.filter(x => x.id === $(id))
-}
-
-
 
 class Producto extends  {
     constructor(title, description, price, thumbnail, code, stock) {
@@ -50,3 +63,5 @@ class Producto extends  {
         this.id = id;
     }
 }
+
+
